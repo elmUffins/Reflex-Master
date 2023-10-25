bd2ada3e36e1f6b2d3b7bd654c4f03b7ffc9929f
Eric Gerzenstein <ericyes81007@gmail.com>
2023-10-25 13:38:33 -0300

    even more minor tweaks
    added change password screen
    (does not work)

---
+++ Reflex Master/Reflex Master/cambiarpass.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Reflex_Master
+{
+    public partial class cambiarpass : UserControl
+    {
+        public cambiarpass()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Reflex Master/Reflex Master/logout.Designer.cs
+++ Reflex Master/Reflex Master/logout.Designer.cs
@@ -35,23 +35,28 @@ namespace Reflex_Master
             this.label1 = new System.Windows.Forms.Label();
             this.logoutbutton = new System.Windows.Forms.Button();
             this.back = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(82, 219);
+            this.label1.Location = new System.Drawing.Point(152, 213);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1231, 85);
+            this.label1.Size = new System.Drawing.Size(1130, 85);
             this.label1.TabIndex = 0;
-            this.label1.Text = "¿Desea cerrar sesión/cambiar de cuenta?";
+            this.label1.Text = "¿Desea cerrar sesión/cambiar de contraseña?";
             // 
             // logoutbutton
             // 
             this.logoutbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logoutbutton.Location = new System.Drawing.Point(619, 371);
+            this.logoutbutton.Location = new System.Drawing.Point(619, 320);
             this.logoutbutton.Name = "logoutbutton";
             this.logoutbutton.Size = new System.Drawing.Size(175, 78);
             this.logoutbutton.TabIndex = 1;
@@ -71,11 +76,68 @@ namespace Reflex_Master
             this.back.UseVisualStyleBackColor = false;
             this.back.Click += new System.EventHandler(this.back_Click_1);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(619, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 78);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Cambiar contraseña";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(664, 557);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 78);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Cambiar contraseña";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(60, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 58);
+            this.button3.TabIndex = 14;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(664, 455);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 78);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Cerrar sesión";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(197, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1130, 85);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "¿Desea cerrar sesión/cambiar de contraseña?";
+            // 
             // logout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.back);
             this.Controls.Add(this.logoutbutton);
             this.Controls.Add(this.label1);
@@ -83,6 +145,13 @@ namespace Reflex_Master
             this.Size = new System.Drawing.Size(1386, 788);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button1;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button logoutbutton;
