e47ce5d77042d18eb5a4f6059d12a3170e644ab5
Eric Gerzenstein <ericyes81007@gmail.com>
2023-10-27 15:59:04 -0300

    Detallito
    Hacemos las cosas mas lindas

--- Reflex Master/Reflex Master/ingame.Designer.cs
+++ Reflex Master/Reflex Master/ingame.Designer.cs
@@ -34,6 +34,16 @@ namespace Reflex_Master
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ingame));
             this.back = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,11 +69,121 @@ namespace Reflex_Master
             this.pictureBox2.TabIndex = 13;
             this.pictureBox2.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(735, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 38);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Esperando...";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightGray;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(735, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(422, 38);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Esperando...";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(735, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(422, 38);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Esperando...";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.LightGray;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(735, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(422, 38);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Esperando...";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.LightGray;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(735, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(422, 38);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Esperando...";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.LightGray;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(735, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(422, 38);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Esperando...";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.LightGray;
+            this.label7.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(735, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(422, 38);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Esperando...";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.LightGray;
+            this.label8.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(735, 457);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(422, 38);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Esperando...";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.LightGray;
+            this.label9.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(735, 516);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(422, 38);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Esperando...";
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.LightGray;
+            this.label10.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(735, 576);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(422, 38);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Esperando...";
+            // 
             // ingame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.back);
             this.Name = "ingame";
@@ -71,6 +191,20 @@ namespace Reflex_Master
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.PictureBox pictureBox2;
 
--- Reflex Master/Reflex Master/ingame.cs
+++ Reflex Master/Reflex Master/ingame.cs
@@ -7,6 +7,7 @@ namespace Reflex_Master
     public partial class ingame : UserControl
     {
         SerialPort serialPort;
+        int receivedDataCount = 0;
         public ingame()
         {
             InitializeComponent();
@@ -15,6 +16,7 @@ namespace Reflex_Master
                 if (myForm.gameon == true)
                 {
                     serialPort = new SerialPort("COM6", 9600);
+                    serialPort.DataReceived += SerialPort_DataReceived; // Add event handler
                     try
                     {
                         serialPort.Open();
@@ -25,9 +27,28 @@ namespace Reflex_Master
                     }
                 }
             }
-            
         }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Assuming the Arduino sends data as comma-separated values
+            string data = serialPort.ReadLine(); // Read a line of data
 
+            if (!string.IsNullOrEmpty(data))
+            {
+                // Split the received data by commas
+                string[] values = data.Split(',');
+
+                if (values.Length == 10 && receivedDataCount < 10)
+                {
+                    // Display the received numbers in labels
+                    // this.Invoke(new Action(() => label1.Text = values[0]));
+                    // this.Invoke(new Action(() => label2.Text = values[1]));
+                    // Repeat for label3, label4, ..., label10
+
+                    receivedDataCount++;
+                }
+            }
+        }
 
         private void back_Click(object sender, EventArgs e)
         {
